1df2a64622731688a40f5dfeef06aff6b0488125
601115 <601115@0338S-S-R0B6H0L.sdcs.local>
2019-09-17 14:23:30 -0700

    Houses (incomplete)

---
+++ src/Houses.java
@@ -0,0 +1,21 @@
+import org.jointheleague.graphical.robot.Robot;
+
+public class Houses {
+	public static void main(String[] args) {
+	 Robot BigSad = new Robot();
+	 BigSad.setX(20);
+	 BigSad.setY(500);
+	 BigSad.setSpeed(100);
+static void 
+	 BigSad.penDown();
+	 BigSad.move(100);
+	 BigSad.turn(90);
+	 BigSad.move(20);
+	 BigSad.turn(90);
+	 BigSad.move(100);
+	 BigSad.turn(270);
+	 BigSad.setPenColor(0, 150, 0);;
+	 BigSad.move(30);
+	 BigSad.setPenColor(100,100,100);
+	}
+}	
