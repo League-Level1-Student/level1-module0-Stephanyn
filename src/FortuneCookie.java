746858c8f43ff8bbf924779e323f70f4e0961d7c
601115 <601115@0338S-Z-90TDE0V.sdcs.local>
2020-01-14 14:21:57 -0800

    fortune cookie

--- src/FortuneCookie.java
+++ src/FortuneCookie.java
@@ -1,7 +1,10 @@
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class FortuneCookie implements ActionListener {
 	public void showButton () {
@@ -12,16 +15,27 @@ public class FortuneCookie implements ActionListener {
 		    frame.add(button);
 		    frame.pack();
 		 button.addActionListener(this);
-		 
-		
+		 int rand = new Random().nextInt(5);		
+		if(rand == 0) {
+			JOptionPane.showMessageDialog(null," Con Mazola nunca estas sola. ");
+		}else if (rand == 1) {
+			JOptionPane.showMessageDialog(null, "If you're feeling upsetti or regretti eat spagetti. ");
+		}else if (rand == 2) {
+			JOptionPane.showMessageDialog(null, "You wot m8?!");
+		}else if (rand == 3) {
+			JOptionPane.showMessageDialog(null, "Having anxiety is like perpetually hearing the boss/enemy music but never seeing the threat. ");
+		}else if (rand == 4) {
+			JOptionPane.showMessageDialog(null, "Cobra chickens are scary");
+		}
 		
 		
 		 
 	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null, "Woohoo!!");
+	}
 }
 
-public class FortuneCookieRunner {
-    public static void main(String[] args) {
-        new FortuneCookie().showButton();
-  }
-}
