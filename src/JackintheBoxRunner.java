d60d054c358d7685067fa3f8b09509294bb8d766
601115 <601115@0338S-Z-90TDE0V.sdcs.local>
2020-01-28 14:20:50 -0800

    Jack in the Box (incomplete) Sound effect machine (incomplete)

---
+++ src/JackintheBoxRunner.java
@@ -0,0 +1,7 @@
+import _08_jack_in_the_box.JackintheBox;
+
+public class JackintheBoxRunner {
+	public static void main (String [] args) {
+		 new JackintheBox().Jack();
+	}
+}
--- src/_08_jack_in_the_box/JackintheBox.java
+++ src/_08_jack_in_the_box/JackintheBox.java
@@ -1,11 +1,13 @@
 package _08_jack_in_the_box;
 
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
-public class JackintheBox {
+public class JackintheBox implements ActionListener {
+	private int clicked = 0;
 	public void Jack() {
 		JFrame frame = new JFrame();
 		frame.setVisible(true);
@@ -14,9 +16,18 @@ public class JackintheBox {
 			frame.add(button);
 			frame.pack();
 			button.addActionListener(this);
-		
+			
+			
+		}
 
+	 
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			// TODO Auto-generated method stub
+			clicked ++;
+		}
+		
+			// TODO Auto-generated method stub
+			
+		}
 	
-	}	
-		int clicked = Integer.parseInt("button");
-	}
